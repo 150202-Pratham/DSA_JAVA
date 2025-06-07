d83a4b5957536939d6f554f19381421398aeca0d
150202-Pratham <140713281+150202-Pratham@users.noreply.github.com>
2025-06-07 12:19:10 +0530

    Changes in the Optimized Code

--- src/arrays/Kadanes_algo/Maximum_Circular_Subarray.java
+++ src/arrays/Kadanes_algo/Maximum_Circular_Subarray.java
@@ -1,4 +1,56 @@
 package arrays.Kadanes_algo;
 
 public class Maximum_Circular_Subarray {
+
+    public int kadanes(int []arr){
+        int total = 0 ;
+        int maxi = Integer.MIN_VALUE ;
+        for(int i = 0 ; i<arr.length ; i++){
+            total+=arr[i] ;
+
+            if(total>maxi){
+                maxi = total;
+            }
+
+            if(total<0){
+                total = 0 ;
+
+            }
+
+
+
+        }
+
+        return maxi ;
+
+
+
+    }
+    public int maxSubarraySumCircular(int[] arr) {
+
+        int sum = 0 ;
+
+        int Normal_Max = kadanes(arr) ;
+
+        for(int i = 0 ; i<arr.length ; i++){
+            sum+=arr[i] ;
+            arr[i] *= -1 ;
+
+
+
+
+        }
+
+        int min_Subarray = kadanes(arr) ;
+
+        int circular_Max = min_Subarray + sum ;
+
+        if(circular_Max == 0) return Normal_Max ;
+
+
+
+
+        return(Math.max(Normal_Max , circular_Max));
+    }
+
 }
