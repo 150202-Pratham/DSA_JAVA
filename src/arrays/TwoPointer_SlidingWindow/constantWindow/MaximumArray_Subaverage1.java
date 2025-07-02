78be79ad0d6c3de5070b4ae751181426a4379a6b
150202-Pratham <140713281+150202-Pratham@users.noreply.github.com>
2025-07-02 10:15:07 +0530

    Constatnt Sliding Window

--- src/arrays/TwoPointer_SlidingWindow/constantWindow/MaximumArray_Subaverage1.java
+++ src/arrays/TwoPointer_SlidingWindow/constantWindow/MaximumArray_Subaverage1.java
@@ -1,4 +1,49 @@
 package arrays.TwoPointer_SlidingWindow.constantWindow;
 
 public class MaximumArray_Subaverage1 {
+    public static  double findMaxAverage(int[] arr, int k) {
+
+        int l = 0 , r = k-1;
+        double average = 0 ;
+
+
+        for(int i = l ; i<=r ; i++){
+
+            average += arr[i];
+
+
+
+        }
+
+       double maxsum = average ;
+
+
+        while(r+1<arr.length-1){
+
+            average = average-arr[l] ;
+            l++ ;
+            r++ ;
+            average = average+arr[r] ;
+
+            maxsum = Math.max(maxsum , average) ;
+
+        }
+
+
+
+        return maxsum/k;
+
+
+    }
+    public static void main(String[] args) {
+
+
+        int []arr = {1,12,-5,-6,50,3};
+        int k = 4 ;
+
+        double output  = findMaxAverage(arr , k) ;
+
+        System.out.printf("%.5f\n" ,output);
+
+    }
 }
