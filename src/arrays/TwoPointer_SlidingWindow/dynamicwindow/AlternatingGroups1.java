6b1f28494ca2a195c2d940ba3b07ce353d94f502
150202-Pratham <140713281+150202-Pratham@users.noreply.github.com>
2025-07-02 10:15:55 +0530

    Dynamic Window

--- src/arrays/TwoPointer_SlidingWindow/dynamicwindow/AlternatingGroups1.java
+++ src/arrays/TwoPointer_SlidingWindow/dynamicwindow/AlternatingGroups1.java
@@ -1,4 +1,32 @@
 package arrays.TwoPointer_SlidingWindow.dynamicwindow;
 
 public class AlternatingGroups1 {
+    public  static int numberOfAlternatingGroups(int[] arr) {
+        int n = arr.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int a = arr[i % n];
+            int b = arr[(i + 1) % n];
+            int c = arr[(i + 2) % n];
+
+            if (a == c && b != a) {
+                count++;
+            }
+        }
+
+        return count;
+
+
+
+
+
+    }
+    public static void main(String[] args) {
+
+        int []arr = {0,1,0,0,1} ;
+
+        System.out.println(numberOfAlternatingGroups(arr));
+
+    }
 }
