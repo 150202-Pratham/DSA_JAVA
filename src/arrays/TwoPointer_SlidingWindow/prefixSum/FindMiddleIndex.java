acba48717160db0eb66831064345a18993c02603
150202-Pratham <140713281+150202-Pratham@users.noreply.github.com>
2025-07-02 10:24:33 +0530

    Find Middle Index.Java

--- src/arrays/TwoPointer_SlidingWindow/prefixSum/FindMiddleIndex.java
+++ src/arrays/TwoPointer_SlidingWindow/prefixSum/FindMiddleIndex.java
@@ -1,4 +1,32 @@
 package arrays.TwoPointer_SlidingWindow.prefixSum;
 
+import java.util.Arrays;
+
 public class FindMiddleIndex {
+
+    public static void middleIndex(int arr[]){
+
+//        Finding the Prefix sum ;
+
+        int []prefixarr = new int[arr.length] ;
+
+        prefixarr[0] = arr[0];
+
+        for(int i = 1 ; i<arr.length; i++){
+
+            prefixarr[i] = prefixarr[i-1]+arr[i] ;
+
+
+        }
+
+        System.out.println(Arrays.toString(prefixarr));
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,3,-1,8,4} ;
+
+        middleIndex(arr) ;
+
+
+    }
 }
